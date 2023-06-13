d28d9161ad1819e53b2715e5d30b3b4237940d84
peto123455 <peto123455@gmail.com>
2023-06-14 00:21:27 +0200

    Added button colors, added question counter and timer

--- src/main/java/sk/uniza/fri/autoskola/TestQuestion.kt
+++ src/main/java/sk/uniza/fri/autoskola/TestQuestion.kt
@@ -1,12 +1,12 @@
 package sk.uniza.fri.autoskola
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
+import android.widget.Button
 import androidx.databinding.DataBindingUtil
 import sk.uniza.fri.autoskola.databinding.TestQuestionBinding
 
@@ -19,9 +19,12 @@ class TestQuestion : Fragment() {
     // onDestroyView.
     private var _binding: TestQuestionBinding? = null
     private var _currentQuestion: Questions.Question? = null
+    private var _timer: CountDownTimer? = null
     private val binding get() = _binding!!
     val currentQuestion get() = _currentQuestion!!
     private val _parent get() = (parentFragment as Test)
+
+    private var answered = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = DataBindingUtil.inflate<TestQuestionBinding>(inflater, R.layout.test_question, container, false)
@@ -29,8 +32,21 @@ class TestQuestion : Fragment() {
         //_binding!!.question = questions[0]
         _binding!!.question = this
         _currentQuestion = _parent.questions[0]
+        _binding?.buttonNext?.isEnabled = false
+        _timer = object: CountDownTimer(1800000, 1000) {
+            override fun onTick(p0: Long) {
+                setTimerText(p0)
+            }
+
+            override fun onFinish() {
+                _parent.finish()
+            }
+        }
+
+        (_timer as CountDownTimer).start()
 
         updateQuestion()
+        resetButtonColors()
 
         return binding.root
     }
@@ -46,10 +62,41 @@ class TestQuestion : Fragment() {
     }
 
     fun selectAnswer(index: Int) {
+        if (_currentQuestion == null || answered) return
+
+        if (index == _currentQuestion!!.answer) _parent.addPoints(_currentQuestion!!.category.points);
+        _parent.answered()
+        answered = true
+
+        revealAnswer(index)
+
+        _binding?.buttonNext?.isEnabled = true
+    }
+
+    private fun revealAnswer(index: Int) {
+        getButtonByIndex(index)?.setBackgroundColor(resources.getColor(R.color.red, null))
+        getButtonByIndex(_currentQuestion!!.answer)?.setBackgroundColor(resources.getColor(R.color.green, null))
+    }
+
+    private fun resetButtonColors() {
+        for (i in 1..3) getButtonByIndex(i)!!.setBackgroundColor(resources.getColor(R.color.babyBlue, null));
+    }
+
+    private fun getButtonByIndex(index: Int): Button? {
+        return when (index) {
+            1 -> _binding!!.buttonA
+            2 -> _binding!!.buttonB
+            3 -> _binding!!.buttonC
+
+            else -> null
+        }
+    }
+
+    fun nextQuestion() {
         if (_currentQuestion == null) return
 
-        _parent.answered()
-        if (index == _currentQuestion!!.answer) _parent.addPoints(_currentQuestion!!.category.points);
+        answered = false
+        resetButtonColors()
 
         if (_parent.questions.size < _parent.answered + 1) {
             _currentQuestion = null;
@@ -60,6 +107,7 @@ class TestQuestion : Fragment() {
         _currentQuestion = _parent.questions[_parent.answered]
 
         updateQuestion()
+        _binding?.buttonNext?.isEnabled = false
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -68,6 +116,16 @@ class TestQuestion : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
+        (_timer as CountDownTimer).cancel()
         _binding = null
+    }
+
+    fun getNumberString(): String {
+        return String.format("Otázka: %d / %d", _parent.answered + 1, _parent.questions.size)
+    }
+
+    fun setTimerText(time: Long) {
+        val seconds: Long = time / 1000
+        _binding!!.questionTimer.text = String.format("%02d:%02d", seconds / 60, seconds % 60)
     }
 }
